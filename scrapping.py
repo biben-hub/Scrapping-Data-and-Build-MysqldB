46c4c0a64b914b6c6d2c47cb30ee28a6d2900bb6
biben-hub <bilombob@gmail.com>
2021-02-11 12:23:13 +0100

    implementing webbrowser

---
+++ scrapping.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+nom_film         = []
+date_sortie_film = []
+durée_film       = []
+catégorie_film   = []
+réalisateur_film = []
+acteurs_film     = []
+note_film        = []
+nationalité_film = []
+image_film       = []
+distributeur     = []
+
+url = "https://www.allocine.fr/films/"
+webbrowser.open_new_tab(url)
