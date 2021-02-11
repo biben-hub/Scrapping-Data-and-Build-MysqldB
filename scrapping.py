b44290192b12a4fe3ac302ffb59745854021b9dc
biben-hub <bilombob@gmail.com>
2021-02-11 17:32:39 +0100

    connection mysql try

--- scrapping.py
+++ scrapping.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
 
 nom_film         = []
 date_sortie_film = []
-durée_film       = []
-catégorie_film   = []
-réalisateur_film = []
+duree_film       = []
+categorie_film   = []
+realisateur_film = []
 acteurs_film     = []
 note_film        = []
-nationalité_film = []
+nationalite_film = []
 image_film       = []
 distributeur     = []
 
@@ -17,7 +20,38 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(class_='meta-title-link')
-elements = results.find_all_next('a', class_='meta-title-link')
-for e in elements:
-    print(e.text, end='\n')
+nom_elements  = results.find_all_next('a', class_='meta-title-link')
+# date_elements = results.find_all_next('a', class_='meta-title-link')
+# duree_elements = results.find_all_next('a', class_='meta-title-link')
+# categorie_elements = results.find_all_next('a', class_='meta-title-link')
+# realisateur_elements = results.find_all_next('a', class_='meta-title-link')
+# acteurs_elements = results.find_all_next('a', class_='meta-title-link')
+# note_elements = results.find_all_next('a', class_='meta-title-link')
+# nationalite_elements = results.find_all_next('a', class_='meta-title-link')
+# image_elements = results.find_all_next('a', class_='meta-title-link')
+# distributeur_elements = results.find_all_next('a', class_='meta-title-link')
 
+
+for e in nom_elements:
+    nom_film.append(e.text)
+print(nom_film)
+
+
+# try:
+my_db = mysql.connector.connect(host     = 'localhost',
+                                user     = 'moi',
+                                password = 'moi',
+                                database = 'films')
+    
+print(my_db)
+#     if my_db.is_connected():
+#         db_info = my_db.get_server_info()
+#         print("Connected to MySQL Server version ", db_info)
+#         cursor = my_db.cursor()
+
+# except Error as e:
+#     print("Error while connecting to MySQL", e)
+# finally:
+#     if (my_db.is_connected()):
+#         my_db.close()
+#         print("MySQL connection is closed")
